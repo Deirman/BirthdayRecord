1eb8f416f7245ee5812ac8f4428e32e2694c0bdb
LCDreamer <liuchao@bogon.dhcp>
2017-07-19 17:03:33 +0800

    写移动端接口

---
+++ BirthdayRecord/src/json/UserInfoCode.java
@@ -0,0 +1,16 @@
+package json;
+
+public class UserInfoCode {
+	public static String CODE_SUCCESS = "ok";
+
+	public static String CODE_ERROR = "0001";
+
+	public static String CODE_ERROR_PARAMETER = "0002";
+
+	public static String CODE_ERROR_PROGRAM = "0003";
+
+	public static String CODE_ERROR_NO_LOGIN_OR_TIMEOUT = "0004";
+
+	public static String CODE_ERROR_EXIST_OPERATION = "0005";
+
+}
